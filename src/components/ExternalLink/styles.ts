d43386c6111db4e8874ee2a740fd471f5e89224e
Soshi <migbonamim@gmail.com>
2022-12-08 15:06:56 -0300

    Botão de voltar

--- src/components/ExternalLink/styles.ts
+++ src/components/ExternalLink/styles.ts
@@ -1,6 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const ExternalLinkContainer = styled.a`
+interface ExternalLinkProps {
+  variant?: 'iconLeft';
+}
+
+export const ExternalLinkContainer = styled.a<ExternalLinkProps>`
   border: none;
   background: none;
   color: ${(props) => props.theme.colors.blue};
@@ -23,4 +27,10 @@ export const ExternalLinkContainer = styled.a`
   &:hover {
     border-color: ${(props) => props.theme.colors.blue};
   }
+
+  ${({ variant }) =>
+    variant === 'iconLeft' &&
+    css`
+      flex-direction: row-reverse;
+    `}
 `;
